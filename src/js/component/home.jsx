2811a2d43d721960dc412254b7125f1ee0038a9d
Solracort <solracort@gmail.com>
2023-01-17 20:27:00 +0000

    ToDoList operativo sin adornar con estilos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,30 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import React, {useState} from "react";
 //create your first component
 const Home = () => {
+	const [input, setInput] = useState("");
+	const [array, setArray] = useState([]);
+
+	function addTask (e){
+		setArray(array.concat(input));
+	}
+	function deleteTask(index){
+		let borrar = array[index]
+		setArray(array.filter((item) => item!=borrar))
+	}
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<ul id="myList">
+				<li>
+					<input onChange={(e)=>setInput(e.target.value)} placeholder="Introduzca una tarea"></input>
+					<button onClick={addTask}>+</button>
+				</li>
+				{array.map((item,index) => (
+					<li key={index}>
+						{item} <button onClick={()=>deleteTask(index)}>x</button>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
